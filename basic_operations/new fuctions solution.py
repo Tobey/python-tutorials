5e8f6029f8d7dc6c40f39562d153704ff49e2d40
obinna ndukwe <obiindukwe@gmail.com>
2021-08-19 16:13:30 +0100

    changed class names

---
+++ basic_operations/new fuctions solution.py
@@ -0,0 +1,178 @@
+"""
+Some starter fucntion
+
+"""
+from basic_operations._xh import check_solution
+
+
+
+
+"""
+write function to count a string
+"""
+def count_a_string(input):
+    a = count_a_string
+    a_count = "a"
+    string.count (string , start_index, end_index,a)
+    print ("number_of_strings_in_a ")
+
+write function to count a string
+"""
+def count_a_list():
+    a = count_a_list
+    a_string.count = [sheila , start_index, end_index, a]
+    a_list = []
+    list_2 = list()
+    print = list_of_strings_in_a_list
+    print = list_of_strings_in_a
+
+
+
+"""
+write a function to reverse a list
+"""
+def reverse_a_list(B):
+
+        return =
+        B= [1o,11,12.13,14,15]
+
+        print  (Reverse(B),
+     B = [1,2,3,4,5,6,]
+    print (ele for ele in reversed('b')
+
+
+
+"""
+write a fucction that multipies number
+"""
+def multiplies_two_numbers(a, b):
+    if a == 2:
+        b ==3
+        return ()
+    print ('6')
+
+    pass
+
+
+"""
+write a function that create a new file called "c" in your present working directory (pwd)
+
+
+"""
+def create_in_pwd():
+    def create_in_desktop():
+        import os
+        path = '//Users//macbookpro//PycharmProjects//python-tutorials//python-tutorials'
+        os.chdir(path)
+        Newfolder = 'test_file.txt'
+        os.makedirs(newfolder)
+    pass
+
+
+"""
+write a function that create a new file called "test_file2.txt" in your present working directory (pwd)
+"""
+
+
+def create_in_desktop():
+    import oss
+    path = '//Users//macbookpro//PycharmProjects//python-tutorials//python-tutorials'
+    os.chdir(path)
+    Newfolder = 'test_file2.txt'
+    os.makedirs(newfolder)
+    pass
+
+
+
+
+"""
+write a function that:
+    - creates a new file called "test_file3.txt" in your current directory
+    - the content of the file should be each number on the input list
+
+
+"""
+def create_in_pwd_with_input():
+    import oss
+    path ='//Users//macbookpro//PycharmProjects//python-tutorials//python-tutorials'
+    f = open(path_to_file, model)
+    with open(test_file3.txt , 'w')as f:
+        f.write('create a new text file!')
+
+
+     pass
+
+
+
+
+"""
+write a function that filters input list and returns only numbers
+
+
+"""
+def create_filter_int(lst)
+   lst = [1,2,3,4,5,6]
+   # filter all elements <4
+
+   small = list(map(lambda x: x if x<4 else None, lst))
+   small = [x for x in small if x!=None]
+   print(small)
+     pass
+
+
+
+"""
+write a function that filters input list and returns only string
+
+"""
+def create_filter_str(sheila):
+    sheila = [1,2,3,4,5,6,7,8,9,10,11]
+def filteroddNum(in_sheila):
+
+    if(in_sheila % 2) == 0:
+     return True
+    else:
+        return  False
+    print("type of filter object: ", type(out_filter))
+    print("filtered seq. is as follows: ",list(out_filter))
+
+
+
+
+
+
+
+"""
+write a function that fetches this api https://api.discogs.com/releases/249504
+it should return data as a string
+"""
+def get_access_code():
+    client_id ='#############'
+    redirect_url = '###########'
+    scope = 'file.readwrite.all offline_access mail.readewrite'
+    url = "" "https://api.discogs.com//releases//249504"
+    import requests
+
+    pass
+
+
+"""
+write a function that fetches this api https://api.discogs.com/releases/249504
+it should return data as a  dict (or json)
+"""
+def api_request_function_json():
+
+    return
+
+"""
+write a function that fetches this api https://api.discogs.com/releases/249504
+it should return all the artist names in a list
+"""
+def api_request_function_vids():
+    pass
+
+
+
+if __name__ == '__main__':
+
+    check_solution()
