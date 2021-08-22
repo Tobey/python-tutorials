847aa3f3269e66de345dc966226782be29506743
Tobey Asinugo <tobey.asinugo@gmail.com>
2021-08-22 21:21:37 +0100

    minor changes to main

--- example/main.py
+++ example/main.py
@@ -7,7 +7,7 @@ class Business:
 
 class Franchise:
 
-    def __init__(self, address, menus):
+    def __init__(self, address, menus, time):
         self.time = time
         self.address = address
         self.menus = menus
@@ -20,19 +20,18 @@ class Franchise:
         available_menu = []
 
         for menu in self.menus:
-            if menu.start_time <= time <= menu:
+            if menu.start_time <= menu.end_time:
                 available_menu.append(menu)
 
             return available_menu
 
 
 class Menu:
-    def __init__(self, name, items, start_time, end_time,time):
+    def __init__(self, name, items, start_time, end_time):
         self.name = name
         self.items = items
         self.start_time = start_time
         self.end_time = end_time
-        self.time = time
 
     def __repr__(self):
 
@@ -49,41 +48,41 @@ class Menu:
 
 if __name__ == '__main__':
 
+    start_time = ""
+    end_time = ""
+
     brunch_items = {'pancakes': 7.50, 'waffles':  9.00, 'burger': 11.00, 'home fries': 4.50,
-                    'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa':10.50,'orange juice': 3.50}
+                    'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50,'orange juice': 3.50}
 
-
-    brunch_menu = Menu('brunch' + brunch_items + str(self.start_time) + str(self.end_time) + 1100  + 1600 )
+    brunch_menu = Menu('brunch', brunch_items, str(start_time), str(end_time))
 
     bill = brunch_menu.calculate_bill(['pancakes', 'home fries', 'coffee'])
 
-
     early_bird_items = {'salumeria plate': 8.00, 'salad and breadsticks (serves 2, no refills)': 14.00,
                         'pizza with quattro formaggi': 9.00, 'duck ragu': 17.50, 'mushroom ravioli (vegan)': 13.50,
-                        'coffee': 1.50, 'espresso': 3.00,}
+                        'coffee': 1.50, 'espresso': 3.00}
 
-    early_bird_menu = Menu('early bird' + early_bird_items + str(self.start_time) + str(self.end_time) + 1500 + 1800)
+    early_bird_menu = Menu('early bird', early_bird_items, str(start_time), str(end_time))
 
-    early_bird_menu.calculate_bill(['salumeria plate', 'mushroom ravioli(vegan)'])
+    early_bird_bill = early_bird_menu.calculate_bill(['salumeria plate', 'mushroom ravioli(vegan)'])
 
+    Dinner_items = {'crostini with eggplant caponata': 13.00, 'ceaser salad': 16.00,
+                    'pizza with quattro formaggi': 11.00,'duck ragu': 19.50, 'mushroom ravioli (vegan)': 13.50,
+                    'coffee': 2.00, 'espresso': 3.00}
 
-    Dinner_items = {'crostini with eggplant caponata': 13.00, 'ceaser salad': 16.00, 'pizza with quattro formaggi': 11.00,
-                    'duck ragu': 19.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 2.00, 'espresso': 3.00}
-    Dinner_menu = Menu('Dinner available' + Dinner_items + str(self.start_time) + str(self.end_time) + 1700  + 2100)
+    Dinner_menu = Menu('Dinner available', Dinner_items, str(start_time), str(end_time))
 
     Kids_items = {'chicken nuggets': 6.50, 'fusilli with wild mushrooms': 12.00, 'apple juice': 3.00}
-    Kids_menu = Menu('brunch' + Kids_items + str(self.start_time) + str(self.end_time) + 1100  + 1600 )
-
+    Kids_menu = Menu('brunch', Kids_items, str(start_time),  str(end_time))
 
     arepas_items = {'arepa pabellon': 7.00, 'pernil arepa': 8.50, 'guayanes arepa': 8.00, 'jamon arepa': 7.50}
-    arepas_menu = Menu('arepas available' + arepas_items + str(self.start_time) + str(self.end_time) + 1000 + 2000  )
+    arepas_menu = Menu('arepas available', arepas_items, str(start_time), str(end_time))
 
+    Menu = [brunch_menu, early_bird_menu, Dinner_menu, Kids_menu, arepas_menu]
 
-    menus = [brunch_menu, early_bird_menu, Dinner_menu, Kids_menu, arepas_menu ]
+    flagship_store = Franchise("1232 West End Road", Menu, 9.00)
 
-    flagship_store = Franchise("1232 West End Road", menus)
-
-    new_installment = Franchise("12 East Mulberry Street", menus)
+    new_installment = Franchise("12 East Mulberry Street", Menu, 11.00)
 
     print(flagship_store)
 
@@ -91,6 +90,8 @@ if __name__ == '__main__':
 
     new_installment.available_menus(1700)
 
-    basta = Business("Basta Fazoolin' with my Heart", [flagship_store, new_installment ])
+    basta = Business("Basta Fazoolin' with my Heart", [flagship_store, new_installment])
 
-    arepas_place = Franchise("189 Fitzgerald Avenue", menus)
+    arepas_place = Franchise("189 Fitzgerald Avenue", Menu, 12.00)
+
+    print('______END_____')
