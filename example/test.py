57d5490d6fa4613a07746b7aae38e349226be301
Tobey Asinugo <tobey.asinugo@gmail.com>
2021-08-23 21:19:19 +0100

    made some chnages

--- example/test.py
+++ example/test.py
@@ -20,22 +20,7 @@ class TestMyExampleClass(TestCase):
     #     self.assertTrue(False)
 
     def test_menu_setup(self):
-        early_bird_items = {
-            'salumeria plate': 8.00,
-            'salad and breadsticks (serves 2, no refills)': 14.00,
-            'pizza with quattro formaggi': 9.00,
-            'duck ragu': 17.50,
-            'mushroom ravioli (vegan)': 13.50,
-            'coffee': 1.50,
-            'espresso': 3.00,
-        }
 
-        menu = Menu("menu 1", early_bird_items, "1000", "1400")
-
-        self.assertEqual(menu.name, "menu 1")
-        self.assertDictEqual(menu.items, early_bird_items)
-        self.assertEqual(menu.start_time, "1000")
-        self.assertEqual(menu.end_time, "1400")
 
         # early_bird_bill = menu.calculate_bill(['sojlrfjodnjksvnk'])
         # self.assertEqual(early_bird_bill, 0)
