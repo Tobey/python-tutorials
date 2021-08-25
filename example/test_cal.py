8c705b4e8d4a72c9db471b4b451dd465071d7b13
obinna ndukwe <obiindukwe@gmail.com>
2021-08-25 08:47:15 +0100

    unittest not running please do check

--- example/test_cal.py
+++ example/test_cal.py
@@ -1,25 +1,14 @@
+import unittest
 import Calc
 from unittest import TestCase
-import Calc
 
-class TestCal(unittest.Tastcase):
+
+import Calc
+class TestCalc(unittest.Tastcase):
 
     def test_add(self):
         result = cal.add(10, 5)
         self.assertEqual(result, 15)
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
