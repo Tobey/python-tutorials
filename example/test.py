586801c524d0d1f88b1185e8a0ce17382cf301d0
obinna ndukwe <obiindukwe@gmail.com>
2021-08-25 10:01:43 +0100

    new correction

--- example/test.py
+++ example/test.py
@@ -8,3 +8,9 @@ class TestCal(unittest.Tastcase):
     def test_add(self):
         result = Calc.add(10, 5)
         self.assertEqual(result, 15)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
