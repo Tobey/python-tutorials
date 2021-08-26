fd37d7c5310c5c56e1d809b5b334b55ecd339d20
obinna ndukwe <obiindukwe@gmail.com>
2021-08-26 18:48:14 +0100

    new updated code

--- calc/employee.py
+++ calc/employee.py
@@ -1,3 +1,4 @@
+import requests
 class Employee:
 
     raise_amt = 1.05
@@ -18,3 +19,10 @@ class Employee:
 
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amt)
+
+    def monthly_schedule(self, month):
+        response = requests.get(f'http://company.com/{self.last}/{month}')
+        if response.ok:
+            return response.text
+        else:
+            return 'Bad Response!'
--- calc/test_employee.py
+++ calc/test_employee.py
@@ -1,16 +1,28 @@
 import unittest
+from unittest.mock import patch
 from employee import Employee
 
 class TestEmployee(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        print('setupclass')
+
+    @classmethod
+    def tearDownClass(cls):
+        print('tearDownClass')
+
     def setUp(self):
-        self.emp_1 = Employee('Corey', 'Schafer', 50000 )
+        print('setup')
+        self.emp_1 = Employee('Corey', 'Schafer', 50000)
         self.emp_2 = Employee('sue', 'Schafer', 60000)
 
+
     def tearDown(self):
-        pass
+        print('tearDown\n')
 
     def test_email(self):
+        print('test_email')
         self.assertEqual(self.emp_1.email, 'Corey.Schafer@email.com')
         self.assertEqual(self.emp_2.email, 'sue.Schafer@email.com')
 
@@ -21,6 +33,7 @@ class TestEmployee(unittest.TestCase):
         self.assertEqual(self.emp_2.email, 'jane.Schafer@email.com')
 
     def test_fullname(self):
+        print('test_fullname')
         self.assertEqual(self.emp_1.fullname, 'Corey Schafer')
         self.assertEqual(self.emp_2.fullname, 'sue Schafer')
 
@@ -31,12 +44,28 @@ class TestEmployee(unittest.TestCase):
         self.assertEqual(self.emp_2.fullname, 'Jane Schafer')
 
     def test_apply_raise(self):
+        print('test_apply_raise')
 
         self.emp_1.apply_raise()
         self.emp_2.apply_raise()
 
         self.assertEqual(self.emp_1.pay, 52500)
         self.assertEqual(self.emp_2.pay, 63000)
+
+    def test_monthly_schedule(self):
+        with patch('employee.requests.get') as mocked_get:
+            mocked_get.return_value.ok = True
+            mocked_get.return_value.ok = 'Success'
+
+            schedule = self.emp_1.monthly_schedule('May')
+            mocked_get.assert_called_with('http://company.com/Schafer/May')
+            self.assertEqual(schedule, 'Success')
+
+            mocked_get.return_value.ok = False
+
+            schedule = self.emp_1.monthly_schedule('June')
+            mocked_get.assert_called_with('http://company.com/Smith/June')
+            self.assertEqual(schedule, 'Bad Resonse')
 
 
 if __name__ == '__main__':
