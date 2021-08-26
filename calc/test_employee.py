82d115f0573e697caa51a885d3ea9de638620a18
obinna ndukwe <obiindukwe@gmail.com>
2021-08-26 09:52:48 +0100

    type error needs correction

--- calc/test_employee.py
+++ calc/test_employee.py
@@ -1,8 +1,64 @@
 import unittest
 from employee import Employee
 
-class TestEmpolyee(unittest.TestCase):
+class TestEmployee(unittest.TestCase):
 
     def test_email(self):
         emp_1 = Employee('Corey', 'Schafer', 50000)
-        emp_2 = Employee('Sue', 'smith', 6000)
+        emp_2 = Employee('Sue', 'Smith', 60000)
+
+        self.assertEqual(emp_1.email, 'Corey.Schafer@email.com')
+        self.assertEqual(emp_2.email, 'sue.smith@email.com')
+
+        emp_1.first = 'john'
+        emp_2.first = 'jane'
+
+        self.assertEqual(emp_1.email, 'john.Schafer@email.com')
+        self.assertEqual(emp_2.email, 'jane.smith@email.com')
+
+    def test_fullname(self):
+        emp_1 = Employee('Corey', 'Schafer', 50000)
+        emp_2 = Employee('Sue', 'Smith', 60000)
+
+        self.assertEqual(emp_1.fullname, 'corey schafer')
+        self.assertEqual(emp_2.fullname, 'sue smith')
+
+        emp_1.first = 'John'
+        emp_2.first = 'Jane'
+
+        self.assertEqual(emp_1.fullname, 'john schafer')
+        self.assertEqual(emp_2.fullname, 'jane smith')
+
+    def test_apply_raise(self):
+        emp_1 = Employee('Corey', 'Schafer', 50000)
+        emp_2 = Employee('Sue', 'Smith', 60000)
+
+        emp_1.apply_raise()
+        emp_2.apply_raise()
+
+        self.assertEqual(emp_1.pay, 52500)
+        self.assertEqual(emp_2.pay, 63000)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
