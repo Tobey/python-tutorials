153c212be69a35505104b13dba9a054d6e51ea0a
obinna ndukwe <obiindukwe@gmail.com>
2021-08-29 11:21:07 +0100

    fin staticts

---
+++ webapi/request.py
@@ -0,0 +1,17 @@
+from math import log
+
+def display_as_percentage(val):
+  return '{:.1f}%'.format(val * 100)
+
+# Write code here
+def calculate_log_return(start_price, end_price):
+  return log(end_price / start_price)
+
+
+log_return = calculate_log_return(200, 250)
+
+print('The log rate of return is', display_as_percentage(log_return))
+
+
+
+
